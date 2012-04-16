a8cd4010c8364a9108d187dd08c0b78313d3bdcd
Stuart <gravatar@strudel.org.uk>
2012-04-16 16:30:04 +0100

    Quick fix for line series

--- js/jquery.graph.js
+++ js/jquery.graph.js
@@ -914,9 +914,13 @@
 				this.canvas.ctx.beginPath();
 				this.canvas.ctx.lineWidth = (this.data[s].lines.lineWidth ? this.data[s].lines.lineWidth : 1);
 				for(var i = 0; i < this.data[s].x.length ; i++){
-					if(this.data[s].x[i] && this.data[s].y[i] && this.data[s].data[i][0] >= this.x.min && this.data[s].data[i][0] <= this.x.max && this.data[s].data[i][1] >= this.y.min && this.data[s].data[i][1] <= this.y.max){
-						if(i == 0) this.canvas.ctx.moveTo(this.data[s].x[i],this.data[s].y[i]);
-						else this.canvas.ctx.lineTo(this.data[s].x[i],this.data[s].y[i]);
+					if(this.data[s].x[i] && this.data[s].y[i]){
+						if(this.data[s].data[i][0] >= this.x.min && this.data[s].data[i][0] <= this.x.max && this.data[s].data[i][1] >= this.y.min && this.data[s].data[i][1] <= this.y.max){
+							if(i == 0) this.canvas.ctx.moveTo(this.data[s].x[i],this.data[s].y[i]);
+							else this.canvas.ctx.lineTo(this.data[s].x[i],this.data[s].y[i]);
+						}else{
+							this.canvas.ctx.moveTo(this.data[s].x[i],this.data[s].y[i]);
+						}
 					}
 				}
 				this.canvas.ctx.stroke();
